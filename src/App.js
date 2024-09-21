f278c3b93be9dce8bf07e18cf901ef25e418db6d
Claudia00022 <klaudiaforysiak@gmail.com>
2024-09-21 14:06:19 +0100

    shoe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
+import { Model } from './Shoe'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+      <Environment preset="forest" />
+      <Model />
+      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
+      <OrbitControls autoRotate />
+    </Canvas>
+  )
 }
-
-export default App;
